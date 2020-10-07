722b7dd51f8dc2e019a5da944c43e4b2b5c504d8
Evelina Sokolovskaya <sokolovskayaevi@gmail.com>
2020-10-07 17:38:01 +0300

    feat: add the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function toReadable (number) {
-  
+  let numbers = ['zero',
+  'one', 
+  'two', 
+  'three', 
+  'four', 
+  'five', 
+  'six', 
+  'seven', 
+  'eight', 
+  'nine', 
+  'ten',    
+        'eleven', 
+        'twelve', 
+        'thirteen', 
+        'fourteen', 
+        'fifteen', 
+        'sixteen', 
+        'seventeen', 
+        'eighteen', 
+        'nineteen',
+                    'twenty', 
+                    'thirty', 
+                    'forty', 
+                    'fifty', 
+                    'sixty', 
+                    'seventy', 
+                    'eighty', 
+                    'ninety'];
+    let result = [];
+    if(number > 99)
+        result.push(numbers[Math.floor(number / 100)] + ' hundred');
+    if(number % 100 != 0){
+        if(number % 100 < 21 )
+            result.push(numbers[number % 100]);
+        else{
+            result.push(numbers[Math.floor((number % 100) / 10) + 18]);
+            if(number % 10 != 0) result.push(numbers[number % 10]);
+        }
+    }
+    return result.join(' ') || 'zero';
 }
+
